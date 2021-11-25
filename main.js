ea7af74b821e885b763225a560cb94c4182d4c20
Králik Csabi <kralikcsabi@gmail.com>
2021-11-25 13:13:30 +0100

    more-js

--- main.js
+++ main.js
@@ -4,21 +4,21 @@ let products = [
     name: "turmix2000",
     price: 5000,
     stock: 3,
-    attachment: ["fej", "motor"]
+    attachments: ["fej", "motor"]
   },
   {
     category: "Pancsi",
     name: "Sampon",
     price: 700,
     stock: 100,
-    attachment: ["Head", "Shoulders"]
+    attachments: ["Head", "Shoulders"]
   },
   {
     category: "Pancsi",
     name: "tusfürdő",
     price: 650,
     stock: 10,
-    attachment: ["kókusz", "szagtalanító"]
+    attachments: ["kókusz", "szagtalanító"]
   }
 ];
 
@@ -29,48 +29,47 @@ function main() {
   let product = products[2];
   let answer = window.confirm("Do you want a " + product.name);
   if (answer != true){
-    let answer = window.confirm("But it jas " + product.attachment.join("."));
+    let answer = window.confirm("But it jas " + product.attachments.join("."));
     if(answer ===true){
       console.log(product);
     }
   }
 }
-function invetory() {
-  let stock = products[2].stock;
-  let price = products[2].price;
+
+function displayDetails(product) {
+  let stock = product.stock;
+  let price = product.price;
   let value = stock * price;
-  let div = document.createElement("div");
-  div.innerText= products[2].name + " : " + value;
-  document.body.append(div);
-
-  stock = products[1].stock;
-  price = products[1].price;
-  value = stock * price;
-  div = document.createElement("div");
-  div.innerText= products[1].name + " : " + value;
-  document.body.append(div);
-
-  stock = products[0].stock;
-  price = products[0].price;
-  value = stock * price;
-  div = document.createElement("div");
-  div.innerText= products[0].name + " : " + value;
-  document.body.append(div);
+  let name = product.name;
+  let attachments = product.attachments;
+  let productDetailsDiv = document.createElement("div");
+  productDetailsDiv.innerText= name + " : " + value + " " + attachments.join(',');
+  productDetailsDiv.className = "products-details";
+  document.getElementById("container").append(productDetailsDiv);
   
-  stock = products[0].stock;
+  
   let available = stock >= 5;
-  div = document.createElement("div");
+  let stockInfoDiv = document.createElement("div");
+  stockInfoDiv.className = "stock-info";
   if(available){
-    div.innerText = products[0].name + " elérhető :D";
+    stockInfoDiv.innerText = name + " elérhető :D";
   } else {
-    div.innerText = products[0].name + "nem elérhető :'c";
+    stockInfoDiv.innerText = name + "nem elérhető :'c";
   }
-  document.body.append(div);
+  productDetailsDiv.append(stockInfoDiv);
 
   let special = customer === "Ádám";
   if (!available && special) {
-    div.innerText = div.innerText + "(de neki mégis)!";
+    stockInfoDiv.innerText = stockInfoDiv.innerText + "(de neki mégis)!";
   }
+}
+
+function invetory() {
+  for (let item of products){
+    displayDetails(item);
+  }
+  
+  
 }
 
 
